5c89e5e2dc9d17128fc8c0fe0e722be8d0718215
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-05-03 13:53:51 +1200

    encrypted username and password

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const crypto = require('crypto')
+const config = require('../config')
+
+const user = new Schema({
+    username: String,
+    password: String,
+    admin: {type: Boolean, default: false}
+})
+
+// Create new user document 
+User.statics.create = function(username, password) {
+    const encrypted = crypto.createHash('sha1', config.secret)
+                    .update(password)
+                    .digest('base64')
+}
