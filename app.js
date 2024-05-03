354d2794c93ccfa9baf218673cd0516f805a673c
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-05-03 13:34:02 +1200

    router setup

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 // Load config
-
-const config 
+const config = require('./config')
 const port = process.env.PORT || 3000
 
 // Express Configuration 
